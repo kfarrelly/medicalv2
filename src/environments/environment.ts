08ec07958bd82b693a337fd329454b2e7a201e9f
Cilans Systems <70142673+Cilans-Systems@users.noreply.github.com>
2020-09-09 19:46:25 +0530

    Update environment.ts
    link globle

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  uri : 'http://localhost:8080',
+  apiuri : "http://localhost:10000",
+  batchUrl : "http://localhost:8008",
 };
 
 /*
