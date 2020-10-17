a82fd1093bccaa21529e7f813e05a1a750183237
jayd <mac@macs-MacBook-Air.local>
2020-10-17 14:06:29 +0530

    environament prod change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  uri : 'http://ec2-34-253-54-125.eu-west-1.compute.amazonaws.com:8080',
+  apiuri : "http://ec2-34-253-54-125.eu-west-1.compute.amazonaws.com:10000",
+  batchUrl : "http://ec2-34-253-54-125.eu-west-1.compute.amazonaws.com:8008",
 };
