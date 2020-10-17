d222a44c297415bbaba98893ef11da70dd0a4ba6
jayd <mac@macs-MacBook-Air.local>
2020-10-17 18:29:06 +0530

    environment change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,12 @@
 
 export const environment = {
   production: false,
-  uri : 'http://localhost:8080',
-  apiuri : "http://localhost:10000",
-  batchUrl : "http://localhost:8008",
+  // uri : 'http://localhost:8080',
+  // apiuri : "http://localhost:10000",
+  // batchUrl : "http://localhost:8008",
+  uri : 'http://ec2-34-253-54-125.eu-west-1.compute.amazonaws.com:8080',
+  apiuri : "http://ec2-34-253-54-125.eu-west-1.compute.amazonaws.com:10000",
+  batchUrl : "http://ec2-34-253-54-125.eu-west-1.compute.amazonaws.com:8008",
 };
 
 /*
