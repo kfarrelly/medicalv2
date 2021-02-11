f55cd4534e53ea0668a9f91d2dc7573168cdbc34
medi-chain <jaydeepkataria@gmail.com>
2021-02-11 22:27:24 +0530

    mangooes changed

--- backend/database/mongooes.js
+++ backend/database/mongooes.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-setTimeout(function() {
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true);
 
-    mongoose.connect('mongodb://localhost:27017/MedicineDB', { useNewUrlParser: true }).
-     catch(error => handleError(error));
+setTimeout(function() {
+    mongoose.connect('mongodb://localhost:27017/MedicineDB', { useNewUrlParser: true ,useUnifiedTopology: true}).
+     catch(error => console.log('mongo err',error));
     }, 60000);
 
 module.exports={mongoose};
