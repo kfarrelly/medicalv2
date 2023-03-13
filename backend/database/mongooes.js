0f595a96d16e62c140a8055a8f1161875b55ab74
nirnaykulshreshtha <nirnaykulshreshtha@gmail.com>
2023-03-13 18:52:39 +0530

    update mongoose connection string using env file

--- backend/database/mongooes.js
+++ backend/database/mongooes.js
@@ -7,7 +7,7 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
 
 setTimeout(function() {
-    mongoose.connect('mongodb://sarafuio:A3H7Vgn2c3g2@docdb-2023-03-09-16-03-46.cluster-c87an2pwav72.eu-west-1.docdb.amazonaws.com:27017/MedicineDB?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false', { useNewUrlParser: true ,useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true ,useUnifiedTopology: true})
       .then(() => console.log('Mongo connected.'))
       .catch(error => console.log('mongo err',error));
     }, 10);
